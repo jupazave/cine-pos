e74bd15e7eeecba0eed8060bbe73db53e7febb29
Juan Pablo Zamora <jupazave@gmail.com>
2017-05-21 16:12:56 -0500

    missing validations

--- app/Http/Middleware/CheckJWT.php
+++ app/Http/Middleware/CheckJWT.php
@@ -20,7 +20,15 @@ class CheckJWT
     public function handle($request, Closure $next)
     {
 
+        if($request->header('authorization') == null) {
+            return response()->json(['error'=>'credentials_required', 'error_description' => 'No authorization token was found']);
+        }
+
         $parts = explode(" ", $request->header('authorization'));
+
+        if(count($parts) != 2){
+            return response()->json(['error'=>'credentials_bad_scheme', 'error_description' => 'Format is Authorization: Bearer [token]']);
+        }
 
         $scheme = $parts[0];
         $credentials = $parts[1];
@@ -28,7 +36,7 @@ class CheckJWT
         if (preg_match("/^Bearer$/i", $scheme)) {
             $token = $credentials;
         } else {
-            return response()->json(['error'=>'credentials_bad_scheme', 'error_description' => 'Format is Authorization: Bearer [token]']);
+            return response()->json(['error'=>'credentials_bad_format', 'error_description' => 'Format is Authorization: Bearer [token]']);
         }
 
         try {
@@ -39,7 +47,7 @@ class CheckJWT
             }else if ($e instanceof \Tymon\JWTAuth\Exceptions\TokenInvalidException) {
                 return response()->json(['token_invalid'], $e->getStatusCode());
             }else{
-                return response()->json(['error'=>'Token is required']);
+                return response()->json(['error'=>'credentials_required', 'error_description' => 'No authorization token was found']);
             }
         }
         $request->attributes->add(['user' => $user]);

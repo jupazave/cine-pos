a73ce143ca71f4290b5be210a1d6b25f2f777463
Juan Pablo Zamora Veraza <jupazave@gmail.com>
2017-05-21 12:30:08 -0500

    Category (#12)
    * Category Crud added
    
    * Fix Readme
    
    * Travis remove xdebug
    
    * Remove xdebug again

--- app/Theater.php
+++ app/Theater.php
@@ -8,4 +8,14 @@ class Theater extends Model
 {
     //
     protected $guarded = [];
+
+    public static $createRules = [
+        'name' => 'required|min:3|max:35',
+        'description' => 'required|min:5',
+    ];
+
+    public static $updateRules = [
+        'name' => 'min:3|max:35',
+        'description' => 'min:5',
+    ];
 }
